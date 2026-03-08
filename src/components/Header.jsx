4e2ea67d4530eb3bc675d51f9123c9580b738709
olegchysh <olegtkachyshyn42@gmail.com>
2026-03-08 17:19:36 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Ткачишин Степан Андрійович</h1>
+      <p>Студент кафедри Захисту інформації</p>
+    </header>
+  );
+}
+export default Header;
